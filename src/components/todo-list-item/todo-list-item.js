1764e6ab94a4c1aaa622c0e592cf22c175ef3bb5
Romario <romaolifirenko5@gmail.com>
2023-09-11 18:40:16 +0300

    final variant

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import "./todo-list-item.css";
+
+export default class TodoListItem extends Component {
+  render() {
+    const {
+      label,
+      onToggleDone,
+      onToggleImportant,
+      important,
+      done,
+      onDelete,
+    } = this.props;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
+
+    return (
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={onToggleDone}>
+          {label}
+        </span>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" onClick={onToggleImportant} />
+        </button>
+        <button
+          onClick={onDelete}
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+}
